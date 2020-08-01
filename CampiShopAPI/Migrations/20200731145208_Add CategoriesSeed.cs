336686b7cd0bdb83b1379d67481713b2487d9262
Bogutlopezc10 <camilopezc@hotmail.com>
2020-08-01 18:13:29 -0500

    Add CategoriesSeed

---
+++ CampiShopAPI/Migrations/20200731145208_Add CategoriesSeed.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CampiShopAPI.Migrations
+{
+    public partial class AddCategoriesSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreatedAt", "Description", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2020, 7, 31, 9, 52, 7, 717, DateTimeKind.Local).AddTicks(3218), "Here you can find all about computers", "Computers", new DateTime(2020, 7, 31, 9, 52, 7, 717, DateTimeKind.Local).AddTicks(9806) },
+                    { 2, new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(172), "Here you can find all about TV's", "TV's", new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(180) },
+                    { 3, new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(187), "Here you can find all about videogames", "Videogames", new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(188) },
+                    { 4, new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(190), "Here you can find all about cell phones", "Cell phones", new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(191) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
---
+++ CampiShopAPI/Migrations/20200731185500_Update CategoriesSeed.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CampiShopAPI.Migrations
+{
+    public partial class UpdateCategoriesSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2020, 7, 31, 13, 54, 59, 922, DateTimeKind.Local).AddTicks(5130), new DateTime(2020, 7, 31, 13, 54, 59, 923, DateTimeKind.Local).AddTicks(1929) });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2020, 7, 31, 13, 54, 59, 923, DateTimeKind.Local).AddTicks(2324), new DateTime(2020, 7, 31, 13, 54, 59, 923, DateTimeKind.Local).AddTicks(2333) });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2020, 7, 31, 13, 54, 59, 923, DateTimeKind.Local).AddTicks(2339), new DateTime(2020, 7, 31, 13, 54, 59, 923, DateTimeKind.Local).AddTicks(2340) });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedAt", "Name", "UpdatedAt" },
+                values: new object[] { new DateTime(2020, 7, 31, 13, 54, 59, 923, DateTimeKind.Local).AddTicks(2342), "Cellphones", new DateTime(2020, 7, 31, 13, 54, 59, 923, DateTimeKind.Local).AddTicks(2343) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2020, 7, 31, 9, 52, 7, 717, DateTimeKind.Local).AddTicks(3218), new DateTime(2020, 7, 31, 9, 52, 7, 717, DateTimeKind.Local).AddTicks(9806) });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(172), new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(180) });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(187), new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(188) });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedAt", "Name", "UpdatedAt" },
+                values: new object[] { new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(190), "Cell phones", new DateTime(2020, 7, 31, 9, 52, 7, 718, DateTimeKind.Local).AddTicks(191) });
+        }
+    }
+}
